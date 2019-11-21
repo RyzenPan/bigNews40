a23007bf64dc6df8394d7692a2004a7ba08a8073
RyzenPan <RyzenPan@github.com>
2019-11-21 14:37:36 +0800

    编写注释

--- admin/js/article_list.js
+++ admin/js/article_list.js
@@ -5,17 +5,14 @@ $(function () {
         type: 'get',
         url: window.BigNew.category_list,
         success: function (res) {
-            // console.log(res);
+            // 通过模版引擎写入到对应option中
             const htmlStr1 = template('list1', res);
             $('#selCategory').html(htmlStr1);
         }
     })
 
 
-
-
-
-    // 渲染所有列表
+    // 封装好一个ajax请求，通过传入page和success函数即可调用
     function list(callback, page = 1) {
         $.get({
             url: BigNew.article_query,
@@ -30,14 +27,15 @@ $(function () {
     }
 
 
+
+    // 请求所有数据并显示第一页，并且加入分页导航栏
     const allList = function (res) {
-        // console.log(res);
+        // 通过模版引擎写入到页面中
         const htmlStr = template('list3', res.data);
-        // console.log(htmlStr);
         $('tbody').html(htmlStr);
         // 引入分页导航栏
         $('#pagination-demo').twbsPagination({
-            totalPages: res.data.totalPage,
+            totalPages: res.data.totalPage,     // 从服务器中获取总页数
             visiblePages: 7,
             first: '首页',
             prev: '上一页',
@@ -48,16 +46,15 @@ $(function () {
                     const htmlStr = template('list3', res.data);
                     $('tbody').html(htmlStr);
                 }
+                // 当用户点击页码的时候重新请求数据并渲染到页面上
                 list(paging, page);
             }
         });
 
     }
 
-
+    // 页面加载时请求所有数据并显示第一页
     list(allList);
-
-
 
     // 删除功能
     $('tbody').on('click', '.delete', function () {
@@ -83,7 +80,9 @@ $(function () {
 
     // 筛选功能
     $('#btnSearch').on('click', function (e) {
+        // 阻止a标签跳转默认行为
         e.preventDefault();
+        // 点击筛选按键时，获取两个select按键的val值，发送ajax请求，返回到数据重新渲染到页面上
         const flter = function (res) {
             $('#pagination-demo').twbsPagination('changeTotalPages', res.data.totalPage, 1)
             const htmlStr = template('list3', res.data);
